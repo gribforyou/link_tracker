fc188f2f8f7e5725a29954bfb35cd6ae260c538b
Ignat <lazovikignat@gmail.com>
2025-02-25 23:20:29 +0300

    Write RepositoryOwner class

--- scrapper/src/main/java/backend/academy/scrapper/repository/RepositoryOwner.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/RepositoryOwner.java
@@ -1,34 +1,54 @@
 package backend.academy.scrapper.repository;
 
+import backend.academy.LinkDto;
+import backend.academy.LinksDto;
+import backend.academy.RemoveLinkDto;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import org.springframework.stereotype.Component;
 
 @Component
 public class RepositoryOwner {
-    private final Set<Integer> chatIds;
-    private final Map<String, Set<Integer>> links;
+    private final Map<Long, Set<LinkDto>> data;
 
     public RepositoryOwner() {
-        this.chatIds = new HashSet<>();
-        this.links = new HashMap<>();
+        this.data = new HashMap<>();
     }
 
-    public void addChatId(int id) {
-        this.chatIds.add(id);
+    public void addChat(long id) {
+        if (!data.containsKey(id)) {
+            data.put(id, new HashSet<>());
+        }
     }
 
-    public void addLink(int id, String link) {
-        if (!chatIds.contains(id)) {
-            throw new IllegalArgumentException("Unknown chat id");
-        }
+    public void removeChat(long id) {
+        data.remove(id);
+    }
 
-        if (!links.containsKey(link)) {
-            links.put(link, new HashSet<>(id));
-        } else {
-            links.get(link).add(id);
+    public void addLink(long id, LinkDto link) {
+        if (!data.containsKey(id)) {
+            throw new IllegalArgumentException("Unknown id");
         }
+        data.get(id).add(link);
+    }
+
+    public LinksDto getLinks(long id) {
+        Set<LinkDto> links = data.get(id);
+        if (links == null) {
+            throw new IllegalArgumentException("Unknown id");
+        }
+        LinkDto[] array = links.toArray(LinkDto[]::new);
+        int length = array.length;
+        return new LinksDto(length, array);
+    }
+
+    public void removeLink(long id, RemoveLinkDto link) {
+        if (!data.containsKey(id)) {
+            throw new IllegalArgumentException("Unknown id");
+        }
+        data.get(id).removeIf(l -> l.url().equals(link.link()));
     }
 }
