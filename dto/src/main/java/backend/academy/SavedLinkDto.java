055b7dc76aef780136f108361c82e215b2323b80
Ignat <lazovikignat@gmail.com>
2025-02-26 22:42:00 +0300

    Solve linter problem

--- dto/src/main/java/backend/academy/SavedLinkDto.java
+++ dto/src/main/java/backend/academy/SavedLinkDto.java
@@ -1,11 +1,6 @@
 package backend.academy;
 
-public record SavedLinkDto(
-    long id,
-    String link,
-    String[] tags,
-    String[] filters
-) {
+public record SavedLinkDto(long id, String link, String[] tags, String[] filters) {
     public static SavedLinkDto of(long id, LinkDto link) {
         return new SavedLinkDto(id, link.link(), link.tags(), link.filters());
     }
--- scrapper/src/main/java/backend/academy/scrapper/repository/RepositoryOwner.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/RepositoryOwner.java
@@ -9,13 +9,12 @@ import backend.academy.scrapper.server.exceptions.UserLinkNotFoundException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RepositoryOwner {
-    private final static long MOCK_ID = 1;
+    private static final long MOCK_ID = 1;
     private final Map<Long, Set<SavedLinkDto>> data;
 
     public RepositoryOwner() {
@@ -59,15 +58,12 @@ public class RepositoryOwner {
         if (!data.containsKey(id)) {
             throw new ChatNotFoundException(id);
         }
-        Optional<SavedLinkDto> first = data.get(id).stream()
-            .filter(l -> l.link().equals(removed.link()))
-            .findFirst();
+        SavedLinkDto linkToRemove = data.get(id).stream()
+                .filter(l -> l.link().equals(removed.link()))
+                .findFirst()
+                .orElseThrow(() -> new UserLinkNotFoundException(removed.link()));
 
-        if (first.isEmpty()) {
-            throw new UserLinkNotFoundException(removed.link());
-        }
-
-        data.get(id).remove(first.get());
-        return first.get();
+        data.get(id).remove(linkToRemove);
+        return linkToRemove;
     }
 }
